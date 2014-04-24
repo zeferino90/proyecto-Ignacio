c708105f8cc35087b09b9d1bc9a2b2eebb4da33c
zeferino90 <mperesp1990@gmail.com>
2014-04-24 12:19:39 +0200

    Nueva clase añadida Modificaciones en la db

--- Facturapp/src/com/index/facturapp/FacturaDB.java
+++ Facturapp/src/com/index/facturapp/FacturaDB.java
@@ -6,10 +6,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class FacturaDB extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "facturas.db";
-	private String sqlCreate = "CREATE TABLE FACTURAS (codigoFactura INTERGER, Fecha DATE)";
+	private String sqlCreate = "CREATE TABLE CLIENTE(dni VARCHAR(10), nombre VARCHAR(30), apellido1 VARCHAR(30), apellido2 VARCHAR(30), direccion VARCHAR(100), localidad VARCHAR(40));" +
+			"CREATE TABLE CATEGORIA (categoria VARCHAR(20));" +
+			"CREATE TABLE PRODUCTO (nombre TEXT, precio FLOAT(8, 2), categoria VARCHAR(20))" +
+			"CREATE TABLE FACTURAS (idFactura INTERGER, fecha DATE, estado VARCHAR(12), cliente VARCHAR(10));" +
+			"CREATE TABLE LINIAPRODUCTO (nombreProducto TEXT, idFactura INTEGER)";
 	
 	public FacturaDB(Context context){
 		super (context, DATABASE_NAME, null, 1);
+	}
+	
+	public getLiniasProducto(int idFactura){
+		//aqui estabas
 	}
 	
 	@Override
@@ -19,7 +27,9 @@ public class FacturaDB extends SQLiteOpenHelper {
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("DROP TABLE IF EXISTS FACTURAS");
+		db.execSQL("DROP TABLE IF EXISTS LINIAPRODUCTO, FACTURAS, PRODUCTO, CATEGORIA, CLIENTE");
 		db.execSQL(sqlCreate);
 	}
+	
+	
 }
---
+++ Facturapp/src/com/index/facturapp/LiniaProducto.java
@@ -0,0 +1,35 @@
+package com.index.facturapp;
+
+public class LiniaProducto {
+	private int factura;
+	private String nombre;
+	private String categoria;
+	private float precio;
+	
+	public int getFactura() {
+		return factura;
+	}
+	public void setFactura(int factura) {
+		this.factura = factura;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+}
