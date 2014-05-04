2f0c1e4ee45491cc40b0d2cde2995533ee2d9ae5
zeferino90 <mperesp1990@gmail.com>
2014-05-04 21:30:38 +0200

    haciendo la UI de gestion de facturas

--- Facturapp/gen/com/index/facturapp/R.java
+++ Facturapp/gen/com/index/facturapp/R.java
@@ -30,14 +30,19 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int anadir=0x7f080003;
+        public static final int action_settings=0x7f080006;
+        public static final int anadir=0x7f080007;
         public static final int contenedor_fact=0x7f080000;
         public static final int expandableListView1=0x7f080001;
+        public static final int nProd=0x7f080003;
+        public static final int nombreProd=0x7f080002;
+        public static final int precioTotal=0x7f080005;
+        public static final int precioUni=0x7f080004;
     }
     public static final class layout {
         public static final int activity_principal=0x7f030000;
         public static final int gestion_facturas=0x7f030001;
+        public static final int liniaprod=0x7f030002;
     }
     public static final class menu {
         public static final int mgestion_facturas=0x7f070000;
