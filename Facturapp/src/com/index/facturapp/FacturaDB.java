74846e75c816eb4726641b37af4e201d82bb61e9
zeferino90 <mperesp1990@gmail.com>
2014-07-03 14:25:41 +0200

    Avanzando con la DB

--- Facturapp/src/com/index/facturapp/FacturaDB.java
+++ Facturapp/src/com/index/facturapp/FacturaDB.java
@@ -1,7 +1,9 @@
 package com.index.facturapp;
 
 import com.index.facturapp.clasesextra.Categoria;
+import com.index.facturapp.clasesextra.Cliente;
 import com.index.facturapp.clasesextra.LiniaProducto;
+import com.index.facturapp.clasesextra.Producto;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -12,11 +14,11 @@ import android.util.Log;
 
 public class FacturaDB extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "facturas.db";
-	private String sqlCreate = "CREATE TABLE CLIENTE(dni VARCHAR(10), nombre VARCHAR(30), apellido1 VARCHAR(30), apellido2 VARCHAR(30), direccion VARCHAR(100), localidad VARCHAR(40))";
-	private String sqlCreate2 =	"CREATE TABLE CATEGORIA (id INTEGER, categoria VARCHAR(20))";
-	private String sqlCreate3 = "CREATE TABLE PRODUCTO (nombre TEXT, precio FLOAT(8, 2), categoria VARCHAR(20))";
-	private String sqlCreate4 = "CREATE TABLE FACTURAS (idFactura INTERGER, fecha DATE, estado VARCHAR(12), cliente VARCHAR(10))";
-	private String sqlCreate5 = "CREATE TABLE LINIAPRODUCTO (nombreProducto TEXT, idFactura INTEGER, cantidad INTEGERc)";
+	private String sqlCreate = "CREATE TABLE CLIENTE(dni VARCHAR(10) PRIMARY KEY, nombre VARCHAR(30), apellido1 VARCHAR(30), apellido2 VARCHAR(30), direccion VARCHAR(100), localidad VARCHAR(40))";
+	private String sqlCreate2 =	"CREATE TABLE CATEGORIA (id INTEGER PRIMARY KEY, categoria VARCHAR(20))";
+	private String sqlCreate3 = "CREATE TABLE PRODUCTO (nombre TEXT PRIMARY KEY, precio FLOAT(8, 2), idcategoria INTEGER)";
+	private String sqlCreate4 = "CREATE TABLE FACTURAS (idFactura INTERGER PRIMARY KEY, fecha DATE, estado VARCHAR(12), cliente VARCHAR(10))";
+	private String sqlCreate5 = "CREATE TABLE LINIAPRODUCTO (nombreProducto TEXT PRIMARY KEY, idFactura INTEGER, cantidad INTEGER)";
 	
 	public FacturaDB(Context context){
 		super (context, DATABASE_NAME, null, 3);
@@ -87,5 +89,55 @@ public class FacturaDB extends SQLiteOpenHelper {
 	    // updating row
 	    db.update("categoria", values, "id" + " = ?",
 	            new String[] { String.valueOf(categoria.getId()) });
+	}
+	
+	public void createCliente (Cliente cliente){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("dni", cliente.getDni());
+		values.put("nombre", cliente.getNombre());
+		values.put("apellido1", cliente.getApellido1());
+		values.put("apellido2", cliente.getApellido2());
+		values.put("direccion", cliente.getDir());
+		values.put("localidad", cliente.getLocalidad());
+		
+		db.insert("cliente", null, values);
+	}
+	
+	public void updateCliente (Cliente cliente) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		 
+	    ContentValues values = new ContentValues();
+	    values.put("nombre", cliente.getNombre());
+		values.put("apellido1", cliente.getApellido1());
+		values.put("apellido2", cliente.getApellido2());
+		values.put("direccion", cliente.getDir());
+		values.put("localidad", cliente.getLocalidad());
+	 
+	    // updating row
+	    db.update("cliente", values, "dni" + " = ?",
+	            new String[] { String.valueOf(cliente.getDni()) });
+	}
+	
+	public void createProducto (Producto producto){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("nombre", producto.getNombre());
+		values.put("precio", producto.getPrecio());
+		values.put("idcategoria", producto.getCategoria().getId());
+		
+		
+		db.insert("producto", null, values);
+	}
+	
+	public void updateProducto (Producto producto){
+		SQLiteDatabase db = this.getWritableDatabase();
+		 
+	    ContentValues values = new ContentValues();
+	    values.put("precio", producto.getPrecio());
+		values.put("idcategoria", producto.getCategoria().getId());
+	 
+	    // updating row
+	    db.update("producto", values, "nombre" + " = ?", new String []{producto.getNombre()});
 	}
 }
