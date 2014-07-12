3d724aa0daa2b99b94b584d5b198ddbaba92ac10
zeferino90 <mperesp1990@gmail.com>
2014-07-12 22:41:52 +0200

    intentando crear la activity para las preferencias pork el R no se genera correctamente

--- Facturapp/src/com/index/facturapp/Preferencias.java
+++ Facturapp/src/com/index/facturapp/Preferencias.java
@@ -2,48 +2,33 @@ package com.index.facturapp;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Principal extends Activity {
+public class Preferencias extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_principal);
-		
-		getFacturas();
-	}
-
-	private void getFacturas() {
-		// TODO Auto-generated method stub
-		
+		setContentView(R.layout.activity_preferencias);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.principal, menu);
-		return super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.preferencias, menu);
+		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.anadir) {
-			Intent intent = new Intent(this, Gestion_facturas.class);
-			intent.putExtra("nuevo", true);
-			//intent.putExtra("factura", factura); esto es para cuando no sea una nueva sino una de las que aprete del listado
-		    startActivity(intent);
+		if (id == R.id.action_settings) {
 			return true;
-		}
-		else if (id == R.id.action_settings){
-			//Intent intent = new Intent(this, Settings.class);
 		}
 		return super.onOptionsItemSelected(item);
 	}
