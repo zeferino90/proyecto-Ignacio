aa8399ff7070916b2c4363299006db21aa5d93b0
zeferino90 <mperesp1990@gmail.com>
2014-07-21 20:19:52 +0200

    inicio de preferencias

--- Facturapp/gen/com/index/facturapp/R.java
+++ Facturapp/gen/com/index/facturapp/R.java
@@ -30,26 +30,27 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int anadir=0x7f080013;
-        public static final int cantidad=0x7f08000e;
-        public static final int confirmar=0x7f080010;
-        public static final int contenedor_fact=0x7f080000;
-        public static final int declinar=0x7f080011;
-        public static final int dialogbuttons=0x7f08000f;
-        public static final int dialogmain=0x7f080006;
-        public static final int dialogprodprecio=0x7f08000a;
-        public static final int escogecategoria=0x7f080007;
-        public static final int escogeproducto=0x7f080009;
-        public static final int introducecantidad=0x7f08000d;
-        public static final int listalinias=0x7f080001;
-        public static final int nProd=0x7f080003;
-        public static final int nombreProd=0x7f080002;
-        public static final int precio=0x7f08000c;
-        public static final int precioTotal=0x7f080005;
-        public static final int precioUni=0x7f080004;
-        public static final int spincategoria=0x7f080008;
-        public static final int spinproducto=0x7f08000b;
+        public static final int action_settings=0x7f080013;
+        public static final int anadir=0x7f080014;
+        public static final int cantidad=0x7f08000f;
+        public static final int cateprod=0x7f080000;
+        public static final int confirmar=0x7f080011;
+        public static final int contenedor_fact=0x7f080001;
+        public static final int declinar=0x7f080012;
+        public static final int dialogbuttons=0x7f080010;
+        public static final int dialogmain=0x7f080007;
+        public static final int dialogprodprecio=0x7f08000b;
+        public static final int escogecategoria=0x7f080008;
+        public static final int escogeproducto=0x7f08000a;
+        public static final int introducecantidad=0x7f08000e;
+        public static final int listalinias=0x7f080002;
+        public static final int nProd=0x7f080004;
+        public static final int nombreProd=0x7f080003;
+        public static final int precio=0x7f08000d;
+        public static final int precioTotal=0x7f080006;
+        public static final int precioUni=0x7f080005;
+        public static final int spincategoria=0x7f080009;
+        public static final int spinproducto=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_preferencias=0x7f030000;
@@ -67,6 +68,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int anadir=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int catprod=0x7f05000e;
         public static final int declinar=0x7f050006;
         public static final int escogecategoria=0x7f050007;
         public static final int escogeproducto=0x7f050008;
--- Facturapp/src/com/index/facturapp/Preferencias.java
+++ Facturapp/src/com/index/facturapp/Preferencias.java
@@ -2,8 +2,8 @@ package com.index.facturapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class Preferencias extends Activity {
 
@@ -11,5 +11,16 @@ public class Preferencias extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_preferencias);
+		
+		TextView catprod = (TextView) findViewById(R.id.cateprod);
+		catprod.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
+	
 }
